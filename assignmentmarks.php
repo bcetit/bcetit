d9b8dc6286be858f493ac08114e374a3d5c84293
bcetit <42590569+bcetit@users.noreply.github.com>
2018-09-04 02:44:56 +0530

    Add files via upload

---
+++ assignmentmarks.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      </head>
+   
+    <body>
+     
+        <div class="container-fluid" id="content">
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-4" id="settings-container">
+                    <h4>Assignment Marks put according to Quesion</h4>
+                    <form method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="q1"  placeholder="Q.1" required>
+                        </div>
+                        <form method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="q2" placeholder="Q.2" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="q3"  placeholder="Q.3" required>
+                        </div>
+						 <div class="form-group">
+                            <input type="text" class="form-control" name="q4"  placeholder="Q.4" required>
+                        </div>
+                        <button input type="submit" name="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+     
+    </body>
+</html>
---
+++ bl_g_m_g6_16.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+include('connection.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!--jQuery library--> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<style>
+tr:hover{
+background-color:#e57373;
+}
+th{
+background-color: #37474F;
+color: #F8F8FF;
+} 
+tbody{
+color: #800000;
+font-size: 20px !important;
+}   
+</style>
+</head>
+<body>
+<?php
+$id=$_SESSION['id'];
+$sql="SELECT * FROM login where id=$id";
+$result_set=mysqli_query($con,$sql);
+if($result_set==true)
+{
+include('profile_header.php');
+include('assignmentmarks.php');
+if(isset($_POST['submit']))
+{  
+$a=$_POST['q1'];
+$b=$_POST['q2'];
+$c=$_POST['q3'];
+$d=$_POST['q4'];
+$sql="INSERT INTO bl_g_m_g6_16(q1,q2,q3,q4) VALUES('$a','$b','$c','$d')";
+$r=mysqli_query($con,$sql);
+if($r==true)
+{
+?>
+<script type="text/javascript"> alert("Marks uploaded sucessfully");
+</script>
+<?php
+}	
+else
+{
+	?>
+	<script type="text/javascript"> alert("Plz try again");
+</script>
+<?php
+}
+}
+}
+include("footer.php");
+?>
